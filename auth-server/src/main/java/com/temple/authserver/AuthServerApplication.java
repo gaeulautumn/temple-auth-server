63c4d463c335055e47bea2d6967c7b1ab1b67909
gaeulautumn <gaeulautumnlee@gmail.com>
2019-04-03 21:27:48 +0900

    fix error

--- auth-server/src/main/java/com/temple/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/temple/authserver/AuthServerApplication.java
@@ -3,9 +3,13 @@ package com.temple.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EntityScan("com.temple.authlib.model")
+@EnableJpaRepositories("com.temple.authlib.repository")
 public class AuthServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServerApplication.class, args);
--- auth-server/src/main/java/com/temple/authserver/users/UserService.java
+++ auth-server/src/main/java/com/temple/authserver/users/UserService.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+
 @Service
 public class UserService implements UserDetailsService{
 
